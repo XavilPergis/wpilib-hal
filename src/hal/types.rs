d89f5c4b7088fb8bd5dcbdaa7e4110723562bc56
XavilPergis <max.duzen@gmail.com>
2017-09-16 18:42:51 -0400

    Rename `handle` module

---
+++ src/hal/types.rs
@@ -0,0 +1,16 @@
+pub type Handle = i32;
+pub type PortHandle = Handle;
+pub type AnalogInputHandle = Handle;
+pub type AnalogOutputHandle = Handle;
+pub type AnalogTriggerHandle = Handle;
+pub type CompressorHandle = Handle;
+pub type CounterHandle = Handle;
+pub type DigitalHandle = Handle;
+pub type DigitalPwmHandle = Handle;
+pub type EncoderHandle = Handle;
+pub type RelayHandle = Handle;
+pub type GyroHandle = Handle;
+pub type InterruptHandle = Handle;
+pub type NotifierHandle = Handle;
+pub type SolenoidHandle = Handle;
+pub type NativeBool = i32;
